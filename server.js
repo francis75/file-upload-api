8dced4fa2e58a327f3a6280abd6e7bf71d6dd342
francis75 <francis750909@gmail.com>
2020-05-18 00:00:46 -0300

    m

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(morgan('dev'));
 app.use(express.static('uploads'));
 
 //start app 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => 
   console.log(`App is listening on port ${port}.`)
